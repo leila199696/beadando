2e1c18d9a0349e3651c2ec2a5b41a114e9183635
leila <leila199696@gmail.com>
2017-10-14 15:48:43 +0200

    javítás 2.0

--- netkaja/src/main/java/com/bead/netkaja/repository/UserRepository.java
+++ netkaja/src/main/java/com/bead/netkaja/repository/UserRepository.java
@@ -8,9 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long>{
-    Optional<User> findByEmail(String email);
+    Optional<User> findByUserName(String username);
 
-    Optional<User> findByUsername(String username);
-
-    Optional<User> findByUsernameAndPassword(String username, String password);
+    Optional<User> findByUserNameAndPassword(String username, String password);
 }
