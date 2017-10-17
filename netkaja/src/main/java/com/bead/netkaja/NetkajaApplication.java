4d34a24c79ea94fb0670e21bd672aca1cf92860d
kkk1997 <fulopkrisztina97@gmail.com>
2017-10-17 16:37:55 +0200

    macska

--- netkaja/src/main/java/com/bead/netkaja/NetkajaApplication.java
+++ netkaja/src/main/java/com/bead/netkaja/NetkajaApplication.java
@@ -1,12 +1,24 @@
 package com.bead.netkaja;
 
+import com.bead.netkaja.model.Allergens;
+import com.bead.netkaja.model.Role;
+import com.bead.netkaja.model.User;
+import com.bead.netkaja.service.imp.ImpAllergensService;
+import com.bead.netkaja.service.imp.ImpUserService;
+import com.bead.netkaja.service.interf.AllergensService;
+import com.bead.netkaja.service.interf.UserService;
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class NetkajaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NetkajaApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(NetkajaApplication.class, args);
+		UserService userService = context.getBean(ImpUserService.class);
+		userService.create(new User("admin","admin", Role.ADMIN));
+
 	}
 }
--- netkaja/src/main/java/com/bead/netkaja/repository/UserRepository.java
+++ netkaja/src/main/java/com/bead/netkaja/repository/UserRepository.java
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-package com.bead.netkaja.repository;
-
-import com.bead.netkaja.model.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Long>{
-    Optional<User> findByUserName(String username);
-
-    Optional<User> findByUserNameAndPassword(String username, String password);
-}
-=======
 package com.bead.netkaja.repository;
 
 import com.bead.netkaja.model.User;
@@ -23,10 +7,10 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserName(String username);
 
     Optional<User> findByUserNameAndPassword(String username, String password);
 }
->>>>>>> d3a942757b25125dbd5d47cbbc43baf08df1d5d1
