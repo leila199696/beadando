dd148478a5fd04f62ca9c3e8b8f577a45c7b9934
leila <leila199696@gmail.com>
2017-11-03 14:02:42 +0100

    Merge remote-tracking branch 'remotes/origin/master' into service-leila
    # Conflicts:
    #	netkaja/src/main/java/com/bead/netkaja/model/Customer.java
    #	netkaja/src/main/java/com/bead/netkaja/repository/AllergensRepository.java
    #	netkaja/src/main/java/com/bead/netkaja/repository/FoodRepository.java
    #	netkaja/src/main/java/com/bead/netkaja/repository/OrderRepository.java
    #	netkaja/src/main/java/com/bead/netkaja/repository/UserRepository.java
    #	netkaja/target/classes/com/bead/netkaja/NetkajaApplication.class
    #	netkaja/target/classes/com/bead/netkaja/model/Allergens.class
    #	netkaja/target/classes/com/bead/netkaja/model/Base.class
    #	netkaja/target/classes/com/bead/netkaja/model/Customer.class
    #	netkaja/target/classes/com/bead/netkaja/model/Food.class
    #	netkaja/target/classes/com/bead/netkaja/model/FoodType.class
    #	netkaja/target/classes/com/bead/netkaja/model/Order.class
    #	netkaja/target/classes/com/bead/netkaja/model/Role.class
    #	netkaja/target/classes/com/bead/netkaja/repository/CustomerRepository.class
    #	netkaja/target/classes/com/bead/netkaja/repository/OrderRepository.class
    #	netkaja/target/maven-status/maven-compiler-plugin/compile/default-compile/createdFiles.lst
    #	netkaja/target/maven-status/maven-compiler-plugin/testCompile/default-testCompile/createdFiles.lst
    #	netkaja/target/test-classes/com/bead/netkaja/NetkajaApplicationTests.class

--- netkaja/src/main/java/com/bead/netkaja/NetkajaApplication.java
+++ netkaja/src/main/java/com/bead/netkaja/NetkajaApplication.java
@@ -2,11 +2,13 @@ package com.bead.netkaja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class NetkajaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NetkajaApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(NetkajaApplication.class, args);
+
 	}
 }
