451643675f70b639c01c5a23af1f7d79bc52f084
leila <leila199696@gmail.com>
2017-12-20 13:03:33 +0100

    h2 javítása

---
+++ src/main/java/com/bead/netkaja/NetkajaApplication.java
@@ -0,0 +1,44 @@
+package com.bead.netkaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+public class NetkajaApplication extends WebMvcConfigurerAdapter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetkajaApplication.class, args);
+		//ConfigurableApplicationContext context = SpringApplication.run(NetkajaApplication.class, args);
+	/*	AllergensService allergensService = context.getBean(ImpAllergensService.class);
+		CustomerService customerService = context.getBean(ImpCustomerService.class);
+		FoodService foodService = context.getBean(ImpFoodService.class);
+		OrderService orderService = context.getBean(ImpOrderService.class);
+
+		//tesztelés create update delete list
+		allergensService.create(new Allergen("Glutén",1));
+		allergensService.create(new Allergen("Rákfélék",2));
+		allergensService.create(new Allergen("Tojás",3));
+		allergensService.create(new Allergen("Hal",4));
+		allergensService.create(new Allergen("Földimogyoró",5));
+		allergensService.create(new Allergen("Szójabab",6));
+		allergensService.create(new Allergen("Tej",7));
+		allergensService.create(new Allergen("Diófélék",8));
+		allergensService.create(new Allergen("Zeller",9));
+		allergensService.create(new Allergen("Mustár",10));
+		Allergen allergens = allergensService.create(new Allergen("Szezámmag",11));
+		allergensService.create(new Allergen("Kén-dioxid",12));
+		allergensService.create(new Allergen("Csillagfürt",13));
+		allergensService.create(new Allergen("Puhatestűek",14));
+
+		System.out.println("Allergének listája");
+		allergensService.list().forEach(System.out::println);
+
+		System.out.println("Zeller keresése");
+		System.out.println(allergensService.findByName("Zeller"));
+
+		System.out.println("Szezámmag törlése");
+		allergensService.delete(allergens);
+		System.out.println(allergensService.findByName("Szezámmag"));*/
+	}
+}
